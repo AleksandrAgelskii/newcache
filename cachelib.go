183f1788d32a984689b30d531e7167cd04e3cb18
Aleksandr Agelskii <linx2005@mail.ru>
2022-07-21 21:46:01 +0300

    comit2

--- cachelib.go
+++ cachelib.go
@@ -1,1 +1,41 @@
 package cache
+
+import (
+	"errors"
+)
+
+type Cache struct {
+	cache map[string]interface{}
+}
+
+func New() *Cache {
+	return &Cache{
+		cache: make(map[string]interface{}),
+	}
+}
+
+func (c Cache) Set(key string, value interface{}) {
+	c.cache[key] = value
+}
+
+func (c Cache) keyExist(key string) bool {
+	_, exists := c.cache[key]
+	return exists
+}
+
+func (c Cache) Get(key string) (interface{}, error) {
+	exists := c.keyExist(key)
+	if !exists {
+		return nil, errors.New("key not found")
+	}
+	return c.cache[key], nil
+}
+
+func (c Cache) Delete(key string) error {
+	exists := c.keyExist(key)
+	if !exists {
+		return errors.New("key not found")
+	}
+	delete(c.cache, key)
+	return nil
+}
